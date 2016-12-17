7d24be237681a1ce36f858f332fbc93cc85a1944
Dima Beznos <beznos.d@gmail.com>
2016-12-18 00:41:46 +0200

    Setup separate test and development environments

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const { ObjectID } = require('mongodb');
 const mongoose = require('./db/mongoose');
 const express = require('express');
@@ -8,7 +10,7 @@ const Todo = require('./models/Todo');
 const User = require('./models/User');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
